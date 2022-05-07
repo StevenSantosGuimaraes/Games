67634757d6f92978b4199a16d143fca526fa8ffd
Steven Note <steven.geeks@gmail.com>
2022-05-07 14:17:08 -0300

    Update file basic main.

--- Estudos/sorting methods/C/main.c
+++ Estudos/sorting methods/C/main.c
@@ -1,93 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAM 100
+// Methods:
+// Bubble_sort;
+// selection_sort;
+// insertion_sort;
+// quick_sort.
 
-int quantity;
-float array[TAM];
+// swap(array, a , b)
 
-// screen and basic function:
-void printValues(float vector[TAM], int quantity);
-int menuMain();
-char programShutDown();
-void cleanScreen();
-
-// bubble sort
-// insertion sort
-// selection sont
-// quick sort
+void clearSystem();
+void pauseSystem();
 
 int main(){
-    char answer = 'n';
-    int option;
-    while(answer != 's' || answer != 'S'){
-        cleanScreen();
-        option = menuMain();
-        switch(option){
-            case 1: //start
-                printf("\nStart array...");
-                break;
-            case 2: //print
-                printf("\nPrint array...");
-                break;
-            case 3: //sort
-                printf("\nSort array values...");
-                break;
-            case 0: //close
-                answer = programShutDown();
-                break;
-        }
-    }
-    cleanScreen();
-    printf("\n=> Program ended successfully! <=");
+    clearSystem();
+    printf("Test.\n");
+    pauseSystem();
 }
 
-void printValues(float vector[TAM], int quantity){
-    int i;
-    for(i = 0; i < quantity; i++){
-        if(i == 0){
-            printf("\nData: { %.2f", vector[i]);
-        }else if(i == quantity - 1){
-            printf("%.2f }.", vector[i]);
-        }else{
-            printf(", %.2f", vector[i]);
-        }
-    }
-}
-
-// bubble sort
-// insertion sort
-// selection sont
-// quick sort
-
-int menuMain(){
-    int option;
-    do{
-        printf("\n|=================================|");
-        printf("\n|  Please, enter a valid option:  |");
-        printf("\n|---------------------------------|");
-        printf("\n|                                 |");
-        printf("\n|  1) Star new array;             |");
-        printf("\n|  2) Print array;                |");
-        printf("\n|  3) Select sort method;         |");
-        printf("\n|                                 |");
-        printf("\n|  0) Exit.                       |");
-        printf("\n|                                 |");
-        printf("\n|=================================|");
-        scanf("%i", &option);
-    }while(option != 0 || option != 1 || option != 2 || option != 3);
-    return option;
-}
-
-char programShutDown(){
-    char option;
-    do{
-        printf("\nDo you want to close the program? (S/N) => ");
-        scanf(" %c", &option);
-    }while(option != 's' || option != 'S' || option != 'n' || option != 'N');
-    return option;
-}
-
-void cleanScreen(){
+void clearSystem(){
     system("clear");
+}
+
+void pauseSystem(){
+    printf("Press any key to continue...");
+    getchar();
 }
